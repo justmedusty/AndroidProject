8e3943eb6c3e26eee0d8cfd11c0e20d03b307616
justmedusty <dustyn_gibb@hotmail.com>
2022-03-22 16:01:12 -0400

    DatabaseHelper constants

--- app/src/main/java/com/cst2335/androidproject/DatabaseHelper.java
+++ app/src/main/java/com/cst2335/androidproject/DatabaseHelper.java
@@ -6,13 +6,27 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    
+
+    public static String DATABASE_NAME = "Database";
+    public static String TABLE_NAME = "RecipeTable";
+    public static int VERSION = 1;
+    public static String KEY_TITLE = "title";
+    public static String KEY_INGREDIENTS = "ingredients";
+    public static String KEY_URL = "url";
+    public static String KEY_ID = "id";
+
+
+
+
+
     public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        
 
     }
 
