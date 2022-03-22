85395062bcb7a64882177daf3de2b893bde579af
justmedusty <dustyn_gibb@hotmail.com>
2022-03-22 15:54:46 -0400

    Database helper , two other activities

---
+++ app/src/main/java/com/cst2335/androidproject/FavouritesActivity.java
@@ -0,0 +1,14 @@
+package com.cst2335.androidproject;
+
+import android.app.Application;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FavouritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/cst2335/androidproject/PopularActivity.java
@@ -0,0 +1,12 @@
+package com.cst2335.androidproject;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PopularActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
