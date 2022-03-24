275bf38728c14d06389e683816b20030b37f80dd
justmedusty <dustyn_gibb@hotmail.com>
2022-03-24 16:40:10 -0400

    Added Api urls in separate file

--- app/src/main/java/com/cst2335/androidproject/ApiService.java
+++ app/src/main/java/com/cst2335/androidproject/ApiService.java
@@ -6,6 +6,6 @@ public class ApiService {
 
     public static final String BaseSearchUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=";
     public static final String appIdApiKey = "&app_id=77b3cee9&app_key=1f638fb97d020f33df4bec25ae109145";
-    public static final String popularUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=popular&app_id=77b3cee9&app_key=1f638fb97d020f33df4bec25ae109145&random=true"
+    public static final String popularUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=popular&app_id=77b3cee9&app_key=1f638fb97d020f33df4bec25ae109145&random=true";
 
 }
