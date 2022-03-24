4be6442dfcc8116e020f1831af52b744df2aadf4
justmedusty <dustyn_gibb@hotmail.com>
2022-03-24 17:37:32 -0400

    updated apiservice

--- app/src/main/java/com/cst2335/androidproject/ApiService.java
+++ app/src/main/java/com/cst2335/androidproject/ApiService.java
@@ -1,5 +1,7 @@
 package com.cst2335.androidproject;
 
+import javax.annotation.Nullable;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ApiService {
@@ -7,5 +9,18 @@ public class ApiService {
     public static final String BaseSearchUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=";
     public static final String appIdApiKey = "&app_id=77b3cee9&app_key=1f638fb97d020f33df4bec25ae109145";
     public static final String popularUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=popular&app_id=77b3cee9&app_key=1f638fb97d020f33df4bec25ae109145&random=true";
+
+
+
+    public String apiCall(@Nullable String searchTerm) throws MalformedURLException {
+
+        URL url;
+
+        if(searchTerm == null){
+            url = new URL(popularUrl);
+        }
+        else url = new URL(BaseSearchUrl + searchTerm + appIdApiKey);
+
+    }
 
 }
