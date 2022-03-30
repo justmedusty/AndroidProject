f367c01b4342391f482c5ca162b331753986d127
chadrocheleau <cjroche77@gmail.com>
2022-03-30 08:13:39 -0400

    Started work on implementing RecipeDetailsFragment. Added layouts and a recipe fragment activity for phones to handle recipe details when on a phone.

--- app/src/main/java/com/cst2335/androidproject/MainActivity.java
+++ app/src/main/java/com/cst2335/androidproject/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.ViewStub;
 
+import androidx.fragment.app.FragmentManager;
+
 import com.cst2335.utilities.BaseNavActivity;
 
 /**
@@ -17,14 +19,24 @@ public class MainActivity extends BaseNavActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base_nav);//setting base layout
 
-        // checking for phone or tablet to apply appropriate layout
-        boolean isPhone = findViewById(R.id.recipe_details_fragment) == null;
+        boolean isPhone = findViewById(R.id.recipe_details_fragment) == null;// know if on phone or not
+
+        FragmentManager fm = getSupportFragmentManager(); // get fragment manager
+
+        //****************** set up tool bar and nav drawer and load this activities layout into stub
+
         setupNavigation("Chad Rocheleau", "Main Activity", "1.0");
-        // get the ViewStub into which this activities layout will be loaded.
         ViewStub stub = (ViewStub) findViewById(R.id.layout_stub);
         stub.setLayoutResource(R.layout.activity_main);
         stub.inflate();
 
+        // *********** if on tablet load the fragment.
+        if (!isPhone) {
+            RecipeDetailsFragment recipeDetails = new RecipeDetailsFragment();
+            fm.beginTransaction()
+                    .replace(R.id.recipe_details_fragment, recipeDetails)
+                    .commit();
+        }
         Log.d("main", "" + isPhone);
     }
 
